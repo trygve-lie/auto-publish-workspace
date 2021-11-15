b06858f9182d98d6791ed6c33401a0d6e397b949
Trygve Lie <trygve.lie@finn.no>
2021-11-15 15:39:11 +0100

    ci: fix ci

--- release.config.cjs
+++ release.config.cjs
@@ -1,24 +1,30 @@
 module.exports = {
-    "plugins": [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      ["@semantic-release/npm", {
-        "tarballDir": "release",
-        "pkgRoot": "packages/lib"
-      }],
-      ["@semantic-release/npm", {
-        "tarballDir": "release",
-        "pkgRoot": "packages/express"
-      }],
-      ["@semantic-release/npm", {
-        "tarballDir": "release",
-        "pkgRoot": "packages/fastify"
-      }],
-      ["@semantic-release/github", {
-        "assets": "release/*.tgz"
-      }],
-      "@semantic-release/git"
-    ],
-    "preset": "angular"
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    ["@semantic-release/npm", {
+      "tarballDir": "release",
+      "pkgRoot": "packages/lib"
+    }],
+    ["@semantic-release/npm", {
+      "tarballDir": "release",
+      "pkgRoot": "packages/express"
+    }],
+    ["@semantic-release/npm", {
+      "tarballDir": "release",
+      "pkgRoot": "packages/fastify"
+    }],
+    ["@semantic-release/github", {
+      "assets": "release/*.tgz"
+    }],
+    ["@semantic-release/git", {
+      "assets": [
+        "packages/lib/package.json", 
+        "packages/express/package.json",
+        "packages/fstify/package.json",
+      ]
+    }]
+  ],
+  "preset": "angular"
 }
